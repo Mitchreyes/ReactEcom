880f6b5538087b64dc0e98bfc59ec8b9bb2e33f7
Mitchreyes <mitchreyes@gmail.com>
2020-03-08 18:18:58 -0700

    developed sign up form and modified formbutton to have gray button case

--- src/components/formFields.js
+++ src/components/formFields.js
@@ -4,7 +4,7 @@ export class FormInput extends Component {
 	render() {
 		const { className, title, type, placeholder, input } = this.props;
 		return (
-			<div className={'${className} form-input'}>
+			<div className={`${className} form-input`}>
 				<label className="form-input__label">{title}</label>
 				<input className="form-input__input" type={type} placeholder={placeholder} {...input} />
 			</div>
@@ -14,10 +14,15 @@ export class FormInput extends Component {
 
 export class FormButton extends Component {
 	render() {
-		const { className, title, type, onClick, input } = this.props;
+		const { className, title, type, onClick, input, short } = this.props;
 		return (
-			<div className={'${className} form-button'}>
-				<button className={`form-button__button`} type={type} {...input} onClick={onClick}>
+			<div className={`${className} form-button`}>
+				<button
+					className={`form-button__button ${short ? 'form-button__gray-button' : ''}`}
+					type={type}
+					{...input}
+					onClick={onClick}
+				>
 					{title}
 				</button>
 			</div>
