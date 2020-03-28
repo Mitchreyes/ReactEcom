aaf6e16b6d0df79f2d0e2078da67a41384422b87
Mitchreyes <mitchreyes@gmail.com>
2020-03-28 12:37:17 -0700

    implemented setPurchaseDetail action creator and reducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,11 +1,21 @@
-import { SET_USER_PURCHASES } from '../actions/types';
+import { SET_USER_PURCHASES, SET_PURCHASE_DETAIL } from '../actions/types';
 
 const INITIAL_STATE = {
-	purchases: []
+	purchases: [],
+	purchaseDetail: []
 };
 
 export default function(state = INITIAL_STATE, action) {
 	switch (action.type) {
+		case SET_PURCHASE_DETAIL:
+			let purchaseDetail;
+			state.purchases.map((purchase) => {
+				purchaseDetail = purchase;
+			});
+			return {
+				...state,
+				purchaseDetail
+			};
 		case SET_USER_PURCHASES:
 			return {
 				...state,
